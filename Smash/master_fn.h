d0c61874f1fda4f4f8582399ebabab4a2c3b00e6
Yi Liu <34199685+yliu634@users.noreply.github.com>
2021-07-07 08:40:04 +0800

    Update master_fn.h

--- Smash/master_fn.h
+++ Smash/master_fn.h
@@ -837,9 +837,9 @@ public:
           }
         }
       }
-      //size *= 4;  // unit: mega bytes //size is the number of blocks (objects)
+      size *= 4;  // unit: mega bytes //size is the number of blocks (objects)
       string tmp = string("Size for storage ") + to_string(did) + ": " + to_string(size);
-      cout << "Size for storage: " << to_string(size) << endl;
+      //cout << "Size for storage: " << to_string(size) << endl;
       my_write(nameServer, Log, tmp);
     } else return false;
     
