765ae0648878df6b10ee8a6c0aeefef1feddb21c
Yi Liu <34199685+yliu634@users.noreply.github.com>
2021-07-09 12:13:46 +0800

    Update storage_node.h

--- Smash/storage_node.h
+++ Smash/storage_node.h
@@ -169,6 +169,7 @@ public:
         *(Locations *) buff.data() = onlyFirst;
         
         my_write(storages[dSId].addrPort, Insert, buff);
+        std::this_thread::yield();
       } else {
         return false;
       }
