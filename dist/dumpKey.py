cce4011b3002c6436222371077dde2c382b02286
Yi Liu <34199685+yliu634@users.noreply.github.com>
2021-10-14 09:33:12 -0700

    Update dumpKey.py

--- dist/dumpKey.py
+++ dist/dumpKey.py
@@ -3,10 +3,12 @@ import os, re, sys
 def main(argv):
     f = open("keys.txt", 'r')
     osd = int(argv[0], 10)
+    N = int(argv[1], 10)
     # print("osd is: "+ str(osd))
     of = open("dump_osd_" + str(osd) + ".txt", 'w')
     key = f.readline()
-    while key:
+    n = 0
+    while key and n < N:
         p = os.popen('ceph osd map test ' + str(key))
         x = p.readlines()
         query = re.search("\[.*?\]", x[0], re.I|re.M)
@@ -16,6 +18,7 @@ def main(argv):
         if osd in x:
             of.write(str(key))
         key = f.readline()
+        n = n + 1
     f.close()
     of.close()
 
