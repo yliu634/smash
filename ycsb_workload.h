2a96afd8f46acd691c2253ed975db2569aab3d84
Yi Liu <34199685+yliu634@users.noreply.github.com>
2021-10-20 16:13:14 -0700

    Update ycsb_workload.h

--- ycsb_workload.h
+++ ycsb_workload.h
@@ -13,7 +13,7 @@ public:
   double updateproportion = 0.5;
   double readmodifywriteproportion = 0;
   Distribution requestdistribution = zipfian;
-  ZipfianGenerator zip(min = 0, max = 1000, zipfianconstant = 0.9);
+  ZipfianGenerator zip(0, 1000, 0.9);
   int parallel = 1;
   string keyfile;
   int id;
